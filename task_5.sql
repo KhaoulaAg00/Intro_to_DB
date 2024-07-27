4200f6d87dcea19305de8c99cdeff0e723bcf2a6
KhaoulaAg00 <khawlaag8@gmail.com>
2024-07-27 20:22:40 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -10,3 +10,4 @@ USE alx_book_store;
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
+SELECT * FROM Customers WHERE customer_id = 1;
