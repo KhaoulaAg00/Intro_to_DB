efb6961ac8d4e1eebd8db37ee3b295f20587755a
KhaoulaAg00 <khawlaag8@gmail.com>
2024-07-27 20:16:32 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -3,12 +3,12 @@ USE alx_book_store;
 
 -- Print the full description of the table 'books'
 SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
+    COLUMN_NAME AS 'Field', 
+    COLUMN_TYPE AS 'Type', 
+    IS_NULLABLE AS 'Null', 
+    COLUMN_KEY AS 'Key', 
+    COLUMN_DEFAULT AS 'Default', 
+    EXTRA AS 'Extra' 
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
